2610d484785c1bc251dbc3e8f7365ea969d4dd2e
João Pedro <meireles872@gmail.com>
2023-08-24 13:54:06 -0300

    maven integração

--- maven.demo/src/main/java/app/Aplicacao.java
+++ maven.demo/src/main/java/app/Aplicacao.java
@@ -1,7 +1,7 @@
 package app;
 
 import java.util.List;
-
+import java.util.Scanner;
 import dao.DAO;
 import dao.UsuarioDAO;
 import model.Usuario;
@@ -10,46 +10,98 @@ public class Aplicacao {
 	
 	public static void main(String[] args) throws Exception {
 		
+		Scanner scanner = new Scanner(System.in);
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		
-		System.out.println("\n\n==== Inserir usuário === ");
-		Usuario usuario = new Usuario(11, "pablo", "pablo",'M');
-		if(usuarioDAO.insert(usuario) == true) {
-			System.out.println("Inserção com sucesso -> " + usuario.toString());
-		}
-		
-		System.out.println("\n\n==== Testando autenticação ===");
-		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("pablo", "pablo"));
+		int escolha = 0;
+		do {
+			System.out.println("\n\n==== Menu ====");
+			System.out.println("1. Listar usuários");
+			System.out.println("2. Inserir usuário");
+			System.out.println("3. Excluir usuário");
+			System.out.println("4. Atualizar senha");
+			System.out.println("5. Sair");
+			System.out.print("Escolha uma opção: ");
 			
-		System.out.println("\n\n==== Mostrar usuários do sexo masculino === ");
-		List<Usuario> usuarios = usuarioDAO.getSexoMasculino();
-		for (Usuario u: usuarios) {
-			System.out.println(u.toString());
-		}
+			escolha = scanner.nextInt();
+			
+			switch (escolha) {
+				case 1:
+					System.out.println("\n\n==== Mostrar usuários === ");
+					List<Usuario> usuarios = usuarioDAO.getOrderByCodigo();
+					for (Usuario u: usuarios) {
+						System.out.println(u.toString());
+					}
+					break;
+					
+				case 2:
+					System.out.println("\n\n==== Inserir usuário === ");
+				    System.out.print("Digite o código do usuário: ");
+				    int codigo = scanner.nextInt();
+				    
+				    scanner.nextLine();
+				    
+				    System.out.print("Digite o login do usuário: ");
+				    String login = scanner.nextLine();
+				    
+				    System.out.print("Digite a senha do usuário: ");
+				    String senha = scanner.nextLine();
+				    
+				    System.out.print("Digite o sexo do usuário (M/F): ");
+				    char sexo = scanner.next().charAt(0);
+				    
+				    Usuario novoUsuario = new Usuario(codigo, login, senha, sexo);
+				    if(usuarioDAO.insert(novoUsuario)) {
+				        System.out.println("Inserção com sucesso -> " + novoUsuario.toString());
+				    } else {
+				        System.out.println("Erro ao inserir usuário.");
+				    }
+				    break;
+					
+				case 3:
+					System.out.print("Digite o código do usuário a ser excluído: ");
+					int codigoExclusao = scanner.nextInt();
+					usuarioDAO.delete(codigoExclusao);
+					System.out.println("Usuário excluído com sucesso.");
+					break;
+					
+				case 4:
+				    System.out.println("\n\n==== Atualizar senha === ");
+				    System.out.print("Digite o código do usuário para atualizar a senha: ");
+				    int codigoAtualizacao = scanner.nextInt();
+				    
+				    scanner.nextLine();
+				    
+				    Usuario usuarioParaAtualizar = usuarioDAO.get(codigoAtualizacao);
+				    
+				    if (usuarioParaAtualizar != null) {
+				        System.out.print("Digite a nova senha do usuário: ");
+				        String novaSenha = scanner.nextLine();
+				        
+				        usuarioParaAtualizar.setSenha(DAO.toMD5(novaSenha));
+				        
+				        if (usuarioDAO.update(usuarioParaAtualizar)) {
+				            System.out.println("Senha atualizada com sucesso para o usuário de código " + codigoAtualizacao);
+				        } else {
+				            System.out.println("Erro ao atualizar a senha.");
+				        }
+				    } else {
+				        System.out.println("Usuário não encontrado.");
+				    }
+				    break;
 
-		System.out.println("\n\n==== Atualizar senha (código (" + usuario.getCodigo() + ") === ");
-		usuario.setSenha(DAO.toMD5("pablo"));
-		usuarioDAO.update(usuario);
+					
+				case 5:
+					System.out.println("Saindo...");
+					break;
+					
+				default:
+					System.out.println("Opção inválida. Escolha novamente.");
+					break;
+			}
+			
+		} while (escolha != 5);
 		
-		System.out.println("\n\n==== Testando autenticação ===");
-		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("pablo", DAO.toMD5("pablo")));		
-		
-		System.out.println("\n\n==== Invadir usando SQL Injection ===");
-		System.out.println("Usuário (" + usuario.getLogin() + "): " + usuarioDAO.autenticar("pablo", "x' OR 'x' LIKE 'x"));
-
-		System.out.println("\n\n==== Mostrar usuários ordenados por código === ");
-		usuarios = usuarioDAO.getOrderByCodigo();
-		for (Usuario u: usuarios) {
-			System.out.println(u.toString());
-		}
-		
-		System.out.println("\n\n==== Excluir usuário (código " + usuario.getCodigo() + ") === ");
-		usuarioDAO.delete(usuario.getCodigo());
-		
-		System.out.println("\n\n==== Mostrar usuários ordenados por login === ");
-		usuarios = usuarioDAO.getOrderByLogin();
-		for (Usuario u: usuarios) {
-			System.out.println(u.toString());
-		}
+		scanner.close();
 	}
 }
